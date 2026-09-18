ac49d4ae0cc5fc73a1d8d5fc39a35930648dd6d2
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-18 23:07:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='mgp-v36-1-payroll-fix-1';
-const CORE=['./','index.html','style.css','app.js','budget.js','history.js','fixkosten.js','payroll.js','storage.js','manifest.json','icon.svg','icon-180.png','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r.ok){const x=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,x));}return r;}).catch(()=>caches.match('./index.html'))));});
+self.addEventListener('fetch',()=>{});
