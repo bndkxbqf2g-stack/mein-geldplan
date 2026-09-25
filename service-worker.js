3f131dd7c151246e5410c9dd1876dfb76c7f5414
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-25 13:40:27 +0200

    v0.99.11 update PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var CACHE_NAME='mein-geldplan-v0.99.10';
+var CACHE_NAME='mein-geldplan-v0.99.11';
 var APP_SHELL=[
-  './','./index.html','./design-refresh.css?v=7','./app.js?v=0.99.10','./manifest.webmanifest','./icon.svg',
+  './','./index.html','./design-refresh.css?v=7','./app.js?v=0.99.11','./manifest.webmanifest','./icon.svg',
   './config/version.js','./config/salary-2026.js',
   './lib/ui.js','./lib/storage.js','./lib/theme-ui.js','./lib/cycle.js','./lib/budget.js','./lib/budget-ui.js',
   './lib/savings.js','./lib/savings-ui.js','./lib/fixed-costs.js','./lib/fixed-cost-overrides.js','./lib/fixed-costs-ui.js','./lib/history-ui.js','./lib/statistics.js',
