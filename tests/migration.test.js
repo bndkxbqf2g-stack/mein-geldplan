259a2163bcbe4196f531d8e336a0260346276c23
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-25 11:05:24 +0200

    v0.99.3 test pending salary backup

--- tests/migration.test.js
+++ tests/migration.test.js
@@ -14,5 +14,11 @@ test('zukünftige unbekannte Backup-Version wird abgewiesen',()=>{
 
 test('Backup-Metadaten enthalten Schema- und App-Version',()=>{
   const snap=createBackupSnapshot();
-  assert.equal(snap.version,3);assert.equal(snap.appVersion,APP_VERSION);assert.ok(Number.isInteger(snap.schemaVersion));
+  assert.equal(snap.version,4);assert.equal(snap.appVersion,APP_VERSION);assert.ok(Number.isInteger(snap.schemaVersion));
+});
+
+
+test('Backup enthält vorgemerkten Lohn getrennt von Transaktionen',()=>{
+  const snap=createBackupSnapshot();
+  assert.ok(Object.prototype.hasOwnProperty.call(snap,'pendingSalary'));
 });
