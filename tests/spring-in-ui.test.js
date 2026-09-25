6157222ff636e3cd50433f9185cd0cb27a0ee737
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-25 22:28:50 +0200

    test: expect payslip comparison UI

--- tests/spring-in-ui.test.js
+++ tests/spring-in-ui.test.js
@@ -3,7 +3,7 @@ import assert from 'node:assert/strict';
 import fs from 'node:fs';
 const html=fs.readFileSync(new URL('../index.html',import.meta.url),'utf8');
 
-test('Gehaltsprognose ist im ersten Neuaufbau auf den Zeitnachweis fokussiert',()=>{
-  for(const id of ['timeReportFiles','timeReportBtn','pTaxableGross','pShiftAllowance','pNight','pSaturday','pSunday','pHoliday','pPayoutDetail']) assert.match(html,new RegExp(`id="${id}"`));
-  for(const id of ['springInDuties','springInHours','springInBtn','payslipBtn']) assert.doesNotMatch(html,new RegExp(`id="${id}"`));
+test('Gehaltsprognose nutzt Zeitnachweise und separate Bezügemitteilungen ohne Einspring-UI',()=>{
+  for(const id of ['timeReportFiles','timeReportBtn','payslipFiles','payslipBtn','payrollControlList','pTaxableGross','pShiftAllowance','pNight','pSaturday','pSunday','pHoliday','pPayoutDetail']) assert.match(html,new RegExp(`id="${id}"`));
+  for(const id of ['springInDuties','springInHours','springInBtn']) assert.doesNotMatch(html,new RegExp(`id="${id}"`));
 });
